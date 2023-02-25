094878c59fae4c6cf3e32c0455c5efb585f3d223
deepakrockz605 <deepakrockz605@gmail.com>
2023-02-25 15:30:56 +0530

    First Commit

--- index.js
+++ index.js
@@ -15,19 +15,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-
 app.listen(PORT, () => {
   console.log(`App Running on Port ${PORT}`);
 });
@@ -35,6 +22,12 @@ app.listen(PORT, () => {
 app.get('/', (req,res) => {
   res.send('Hello Deepak')
 })
+
+app.all('/', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 var Users = require("./controllers/users");
 app.use("/users", Users);
