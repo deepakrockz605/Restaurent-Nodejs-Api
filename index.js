b827c9f63d7c0c5bf5cf44a6ce36f3e66002dcad
deepakrockz605 <deepakrockz605@gmail.com>
2023-02-25 14:17:06 +0530

    All changes

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 db.connect((err) => {
   if (err) throw err;
   console.log("Database Connected");
